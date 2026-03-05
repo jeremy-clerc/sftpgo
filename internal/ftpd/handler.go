c61da1bc2eef8eaa1617380ecb0f3aa6959c4c15
Jeremy Clerc <j.clerc@criteo.com>
2026-03-05 23:52:28 +0100

    extend in memory stream for ftp too

--- internal/ftpd/handler.go
+++ internal/ftpd/handler.go
@@ -418,7 +418,8 @@ func (c *Connection) handleFTPUploadToNewFile(fs vfs.Fs, flags int, resolvedPath
 		c.Log(logger.LevelDebug, "upload for file %q denied by pre action: %v", requestPath, err)
 		return nil, ftpserver.ErrFileNameNotAllowed
 	}
-	file, w, cancelFn, err := fs.Create(filePath, flags, c.GetCreateChecks(requestPath, true, false))
+	checks := c.GetCreateChecks(requestPath, true, false) | vfs.CheckStreamWrite
+	file, w, cancelFn, err := fs.Create(filePath, flags, checks)
 	if err != nil {
 		c.Log(logger.LevelError, "error creating file %q, flags %v: %+v", resolvedPath, flags, err)
 		return nil, c.GetFsError(fs, err)
@@ -473,7 +474,11 @@ func (c *Connection) handleFTPUploadToExistingFile(fs vfs.Fs, flags int, resolve
 		}
 	}
 
-	file, w, cancelFn, err := fs.Create(filePath, flags, c.GetCreateChecks(requestPath, false, isResume))
+	checks := c.GetCreateChecks(requestPath, false, isResume)
+	if !isResume {
+		checks |= vfs.CheckStreamWrite
+	}
+	file, w, cancelFn, err := fs.Create(filePath, flags, checks)
 	if err != nil {
 		c.Log(logger.LevelError, "error opening existing file, flags: %v, source: %q, err: %+v", flags, filePath, err)
 		return nil, c.GetFsError(fs, err)
